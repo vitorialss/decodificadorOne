dbbbbc8b0fe88bb2a24f846746bcfbf2da4bc03d
vitorialss <107154247+vitorialss@users.noreply.github.com>
2023-04-26 22:28:48 -0300

    "responsividade ajustada"

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const copyButton = document.querySelector("#copy_button");
 const image = document.querySelector(".figure");
 const asideTexts = document.querySelector(".texts");
 const container = document.querySelector(".aside-container");
+const encryptButton = document.querySelector("#encrypt_button");
+const decryptButton = document.querySelector("#decrypt_button");
 
 
 
@@ -15,15 +17,33 @@ inputText.addEventListener("input", () => {
   container.style.height = "100%";
   container.style.justifyContent = "space-between";
   copyButton.style.display = "block";
-
-
-
-  const value = inputText.value;
-  outputText.value =  inputText.value;
-
-  const result = value.replaceAll("e", "enter").replaceAll("i", "imes").replaceAll("a", "ai").replaceAll("o", "ober").replaceAll("u", "ufat");
-
-  console.log(result);
-
-  
+  outputText.style.height = "200px";
 });
+
+function toNormalForm(text) {
+  const lowerCaseText = text.toLowerCase();
+  return lowerCaseText.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+}
+ 
+ encryptButton.addEventListener("click", () => {
+  let value = inputText.value;
+  let normalizeValue  = toNormalForm(value)
+  const result = normalizeValue.replaceAll("e", "enter").replaceAll("i", "imes").replaceAll("a", "ai").replaceAll("o", "ober").replaceAll("u", "ufat");
+  outputText.value =  result;
+
+});
+
+decryptButton.addEventListener("click", () => {
+  let value = inputText.value;
+  let normalizeValue  = toNormalForm(value)
+  const textResult = normalizeValue.replaceAll("enter", "e").replaceAll("imes", "i").replaceAll("ai", "a").replaceAll("ober", "o").replaceAll("ufat", "u");
+  outputText.value = textResult;
+})
+
+copyButton.addEventListener("click", () => {
+
+  navigator.clipboard.writeText(outputText.value);
+
+  inputText.value = "";
+
+})
